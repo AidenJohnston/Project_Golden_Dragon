ce5fd6b4122a212da90f8f0816e776d0a8d16c4c
AidenJohnston <aidendj@icloud.com>
2026-01-17 16:50:48 -0600

    outlining main.py now

---
+++ main.py
@@ -0,0 +1,27 @@
+#here we go
+
+
+#inputs
+budget = input("Enter your budget: $ ")
+risk_tolerance = input("Enter your risk tolerance (low, medium, high): ")
+holding_horizon = input("Enter your holding horizon (quick-flip (0) or hold (1)): ")
+
+
+#turning the risk tolerance into alpha
+if risk_tolerance == "low":
+    alpha = 1.5
+elif risk_tolerance == "medium":
+    alpha = 1.0
+elif risk_tolerance == "high":
+    alpha = 0.5
+
+#get the entirety of the bazaar
+
+#number crunching
+
+#scoring function
+
+#output (top 5 opportunities)
+#Item Tag, Expected Return, Volatility, Fill Probability, and Suggested Action
+
+
