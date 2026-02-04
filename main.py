4c4585a47aa6e58cdedc20d3ccb48ac072d58888
AidenJohnston <aidendj@icloud.com>
2026-02-04 10:45:00 -0600

    cleaned bazaar data for processing

--- main.py
+++ main.py
@@ -3,34 +3,38 @@ import pandas as pd
 import numpy as np
 import requests
 
-#inputs
-budget = input("Enter your budget: $ ")
-risk_tolerance = input("Enter your risk tolerance (low, medium, high): ")
-holding_horizon = input("Enter your holding horizon (quick-flip (0) or hold (1)): ")
+###inputs
+#budget = input("Enter your budget: $ ")
+#risk_tolerance = input("Enter your risk tolerance (low, medium, high): ")
+#holding_horizon = input("Enter your holding horizon (quick-flip (0) or hold (1)): ")
 
 
 #turning the risk tolerance into alpha
-if risk_tolerance == "low":
+"""if risk_tolerance == "low":
     alpha = 1.5
 elif risk_tolerance == "medium":
     alpha = 1.0
 elif risk_tolerance == "high":
-    alpha = 0.5
+    alpha = 0.5"""
 
 #get the entirety of the bazaar
 bazaar_url = "https://api.hypixel.net/v2/skyblock/bazaar"
 items = bf.get_bazaar_list(bf.get_bazaar_data(bazaar_url))
 
-# clear out the useless items
-#i'll do this one later
+# clear out the invalid items
+def invalid_item(item):
+    return (item.get('sellPrice') != 0) & (item.get('buyPrice') != 0)
+
+cleanList = list(filter(invalid_item, items))
+
+for item in cleanList:
+    print(item.get('productId'))
+    print("\tBuy:", item.get('buyPrice'))
+    print("\tSell:", item.get('sellPrice'))
 
 ###number crunching
 #bid/ask spread
 bidAsk_spread = []
-for item in items:
-    spread = bf.bidAskSpread(item)
-    print(spread)
-    bidAsk_spread.append(spread)
 
 
 #liquidity
